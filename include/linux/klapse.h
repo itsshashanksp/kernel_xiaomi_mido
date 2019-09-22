c6f35dc48fab1ec7343739f23e17d74920d3d2cb
Tanish Manku <tanish2k09.dev@gmail.com>
2019-09-22 10:34:59 -0400

    drivers: video: Introduce K-lapse v5.0
    K-lapse is a linear RGB scaling module that 'shifts' RGB based on
    time or brightness. This concept is inspired from LineageOS ROM's
    feature "livedisplay" which also changes the display settings (RGB,
    hue, temperature, etc) based on time.

--- include/linux/klapse.h
+++ include/linux/klapse.h
@@ -1,8 +1,11 @@
 #ifndef _KLAPSE_H
 #define _KLAPSE_H
 
-/* KLAPSE_MDSS : Use 1 if using with MDSS */
+#ifdef CONFIG_FB_MSM
 #define KLAPSE_MDSS 1
+#else
+#define KLAPSE_MDSS 0
+#endif
 
 /* set_rgb_slider : Interface function for brightness-mode */
 typedef int bl_type_t;
@@ -40,7 +43,5 @@ typedef unsigned short col_type_t;
 #define UPPER_BL_LVL 400 /* Upper target for brightness-dependent mode */
 
 #define LOWER_BL_LVL 2 /* Lower target for brightness-dependent mode */
-
-#define DEFAULT_FLOW_FREQ 360 /* Flow delays for rapid pushes in mode 2 */
 
 #endif  /* _KLAPSE_H */
